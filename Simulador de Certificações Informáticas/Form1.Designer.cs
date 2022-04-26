129c5c4e3834fc612de0982bae51b5f82b1f49f0
Danitek <vareladani34@gmail.com>
2022-04-26 13:01:14 +0100

    Add project files.

---
+++ Simulador de Certificações Informáticas/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Simulador_de_Certificações_Informáticas
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBuser = new System.Windows.Forms.TextBox();
+            this.TBpassword = new System.Windows.Forms.TextBox();
+            this.BTNLogin = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(354, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 44);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(100, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Member Login";
+            // 
+            // TBuser
+            // 
+            this.TBuser.BackColor = System.Drawing.Color.Gray;
+            this.TBuser.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBuser.ForeColor = System.Drawing.Color.White;
+            this.TBuser.Location = new System.Drawing.Point(52, 276);
+            this.TBuser.Multiline = true;
+            this.TBuser.Name = "TBuser";
+            this.TBuser.Size = new System.Drawing.Size(307, 40);
+            this.TBuser.TabIndex = 3;
+            this.TBuser.Text = "Username";
+            // 
+            // TBpassword
+            // 
+            this.TBpassword.BackColor = System.Drawing.Color.Gray;
+            this.TBpassword.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBpassword.ForeColor = System.Drawing.Color.White;
+            this.TBpassword.Location = new System.Drawing.Point(52, 349);
+            this.TBpassword.Multiline = true;
+            this.TBpassword.Name = "TBpassword";
+            this.TBpassword.Size = new System.Drawing.Size(258, 40);
+            this.TBpassword.TabIndex = 4;
+            this.TBpassword.Text = "Password";
+            // 
+            // BTNLogin
+            // 
+            this.BTNLogin.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BTNLogin.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNLogin.ForeColor = System.Drawing.Color.White;
+            this.BTNLogin.Location = new System.Drawing.Point(135, 424);
+            this.BTNLogin.Name = "BTNLogin";
+            this.BTNLogin.Size = new System.Drawing.Size(148, 42);
+            this.BTNLogin.TabIndex = 5;
+            this.BTNLogin.Text = "Login";
+            this.BTNLogin.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Simulador_de_Certificações_Informáticas.Properties.Resources.NotVisible;
+            this.pictureBox3.Location = new System.Drawing.Point(316, 349);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(43, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(411, 595);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.BTNLogin);
+            this.Controls.Add(this.TBpassword);
+            this.Controls.Add(this.TBuser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBuser;
+        private System.Windows.Forms.TextBox TBpassword;
+        private System.Windows.Forms.Button BTNLogin;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Simulador de Certificações Informáticas/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulador_de_Certificações_Informáticas
+{
+    public partial class LoginPage : Form
+    {
+        bool isVisible = false;
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if(isVisible == false)
+                isVisible = true;
+            else
+            isVisible = false;
+
+            if (isVisible == false)
+            {
+                pictureBox3.Image = Image.FromFile("media/NotVisible.png");
+            }
+            else
+                pictureBox3.Image = Image.FromFile("media/Visible.png");
+            
+        }
+    }
+}
